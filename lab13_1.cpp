220517b9aadcd0402c3f81eca62aeafb315dc424
tie <tie.teiroom1234@gmail.com>
2024-12-27 20:29:32 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,46 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+
+    double sum = 0;
+    for(int i = 0;i < N;i++){
+        sum = sum + A[i];
+    }
+
+    B[0] = sum / N;
+    
+    double variance = 0;
+    for(int i = 0;i < N;i++){
+        variance = variance + pow(A[i]-B[0],2);
+    }
+    B[1] = sqrt(variance / N);
+
+    double result = 1.0;
+    for(int i = 0;i < N;i++){
+        result *= A[i];
+    }
+    B[2] = pow(result , 1.0/N);
+
+    double harmonic = 0;
+    for(int i = 0;i < N;i++){
+        harmonic += 1/A[i];
+    }
+    B[3] = N / harmonic;
+
+    double max = A[0],min = A[0];
+    for(int i = 1;i < N;i++){
+        if(A[i] > max){
+            max = A[i];
+        }
+        else if(A[i] < min){
+            min = A[i];
+        }
+    }
+    B[4] = max;
+    B[5] = min;
+
+
+
+}
